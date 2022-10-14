27d8b2485a2253f805dce5b4480d9029c9a46dfb
nikita <nikitachertkov@gmail.com>
2022-10-14 23:31:02 +0500

    method indexOfPosition name - new. size++ into base class

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -8,6 +8,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void insertResume(Resume r, int indexOfPosition) {
+        // TODO: 14.10.2022 Сдвиги делай при помощи System.arraycopy - это намного эффективнее работает чем сдвиг при помощи циклов
         int index = -indexOfPosition - 1;
 
         if (storage[index] != null) {
@@ -15,10 +16,8 @@ public class SortedArrayStorage extends AbstractArrayStorage {
                 storage[i + 1] = storage[i];
             }
             storage[index] = r;
-            size++;
         } else {
             storage[index] = r;
-            size++;
         }
     }
 
