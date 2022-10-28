b00e5128b59236c1be235395c8d2d2589591a541
nikita <nikitachertkov@gmail.com>
2022-10-28 15:49:04 +0500

    todo

--- test/com/urise/webapp/storage/ListStorageTest.java
+++ test/com/urise/webapp/storage/ListStorageTest.java
@@ -65,7 +65,7 @@ public class ListStorageTest {
 
     @Test
     public void getAll() {
-        Resume[] expected = new Resume[]{RESUME_3, RESUME_2, RESUME_1}; // TODO: 28.10.2022 точно так же как в мапе порядок кривой 
+        Resume[] expected = new Resume[]{RESUME_3, RESUME_2, RESUME_1};
         assertSize(3);
         assertArrayEquals(expected, storage.getAll());
     }
