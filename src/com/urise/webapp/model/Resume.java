6fb6a713616ace9d2a153a0a581a145566025597
nikita <nikitachertkov@gmail.com>
2022-11-08 14:11:28 +0500

    hw7 preparing

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -7,18 +7,21 @@ import java.util.UUID;
  * Initial resume class
  */
 public class Resume {
+//        implements Comparable<Resume{
 
     // Unique identifier
     private final String uuid;
     private final String fullName;
 
-    public Resume(String uuid, String fullName) {
-        this.uuid = uuid;
-        this.fullName = fullName;
-    }
-
     public Resume(String fullName) {
         this(UUID.randomUUID().toString(), fullName);
+    }
+
+    public Resume(String uuid, String fullName) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
+        this.uuid = uuid;
+        this.fullName = fullName;
     }
 
     public String getUuid() {
@@ -54,4 +57,10 @@ public class Resume {
                 ", fullName='" + fullName + '\'' +
                 '}';
     }
+
+//    @Override
+//    public int compareTo(Resume o) {
+//        int cmp = fullName.compareTo(o.fullName);
+//        return cmp!=0? cmp:uuid.compareTo(o.uuid);
+//    }
 }
