600d692c836486fe87576ff2587d64fe6dc9228e
Nikita <nikitachertkov@gmail.com>
2022-12-04 19:07:40 +0500

    DataStreamSerializer do write

--- src/com/urise/webapp/model/SectionType.java
+++ src/com/urise/webapp/model/SectionType.java
@@ -3,8 +3,8 @@ package com.urise.webapp.model;
 public enum SectionType {
     PERSONAL("Личные качества"), //textSection
     OBJECTIVE("Позиция"),       //textSection
-    ACHIEVEMENT("Достижения"),  //textSection
-    QUALIFICATIONS("Квалификация"), //textSection
+    ACHIEVEMENT("Достижения"),  //ListSection
+    QUALIFICATIONS("Квалификация"), //ListSection
     EXPERIENCE("Опыт работы"),  //organizationSection
     EDUCATION("Образование"); //organizationSection
 
