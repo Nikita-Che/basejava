b65bfca5c079d93cbe152e095f07d4221a807980
nikita <nikitachertkov@gmail.com>
2023-01-25 13:50:48 +0500

    confix fixed

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -11,18 +11,18 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final File PROPS = new File("C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\config\\resumes.properties");
     private static final Config INSTANCE = new Config();
-    protected static final File PROPS = new File("C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\config\\resumes.properties");
     private final File storageDir;
-    public Properties props = new Properties();
-    public Storage storage;
+    private final Storage storage;
 
     public static Config get() {
         return INSTANCE;
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream("C:\\Users\\nikita\\Desktop\\GitHub\\basejava\\config\\resumes.properties")) {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(new SqlHelper(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password")));
